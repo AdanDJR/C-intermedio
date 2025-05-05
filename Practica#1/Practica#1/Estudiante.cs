bc35dbb8a171e8e653d85f13f36d93b0126e5510
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-05-05 00:10:16 -0400

    pratica OPP completada

---
+++ Practica#1/Practica#1/Estudiante.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_OOP
+{
+    class Estudiante : Persona
+    {
+        private string Carrera;
+        private string Matricula;
+        public Estudiante(string nombre,string ciudad,int edad, string carrera,string matricula)
+        : base(nombre,ciudad,edad)
+        {
+            this.Carrera = carrera;
+            this.Matricula = matricula;
+        }
+        public override void InformacionPersona()
+        {
+            string Informacion = this.Nombre + " " + this.Ciudad + " " + this.Edad + " " + this.Carrera + " " + this.Matricula;
+            Console.WriteLine(Informacion);
+        }
+
+    }
+}
