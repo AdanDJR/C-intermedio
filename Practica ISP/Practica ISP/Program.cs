5862fdabc19f3994d222a3c2eb863efb4fed34f0
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-05-05 01:24:09 -0400

    Practica ISP completada

---
+++ Practica ISP/Practica ISP/Program.cs
@@ -0,0 +1,25 @@
+﻿using Practica_ISP;
+
+class Canario : IAve
+{
+    public void Volar() => Console.WriteLine("El canario vuela.");
+    public void Cantar() => Console.WriteLine("El canario canta.");
+    public void PonerHuevos() => Console.WriteLine("El canario pone huevos.");
+}
+
+class Pato : IAve, IAveNadadora
+{
+    public void Volar() => Console.WriteLine("El pato vuela.");
+    public void Cantar() => Console.WriteLine("El pato grazna.");
+    public void PonerHuevos() => Console.WriteLine("El pato pone huevos.");
+    public void Nadar() => Console.WriteLine("El pato nada.");
+}
+
+class Aguila : IAve, IAveCazadora
+{
+    public void Volar() => Console.WriteLine("El águila vuela.");
+    public void Cantar() => Console.WriteLine("El águila grita.");
+    public void PonerHuevos() => Console.WriteLine("El águila pone huevos.");
+    public void Cazar() => Console.WriteLine("El águila caza.");
+}
+
