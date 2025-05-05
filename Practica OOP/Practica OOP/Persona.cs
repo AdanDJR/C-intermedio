e8f21a8eea5e67c0200809384b9e2bbd85ebf0b3
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-05-05 00:02:05 -0400

    Tarea completada

---
+++ Practica OOP/Practica OOP/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_OOP
+{
+    class Persona
+    {
+        protected string Nombre;
+        protected string Ciudad;
+        protected int Edad;
+        public Persona(string nombre, string ciudad, int edad)
+        {
+            this.Nombre = nombre;
+            this.Ciudad = ciudad;
+            this.Edad = edad;
+        }
+        public virtual void InformacionPersona()
+        {
+            string Informacion = this.Nombre + this.Ciudad + this.Edad;
+            Console.WriteLine(Informacion);
+
+        }
+    }
+}
---
+++ Practica OOP/Practica OOP/Rectángulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_OOP
+{
+    class Rectángulo : Figura
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public Rectángulo(double b, double h)
+        {
+            Base = b;
+            Altura = h;
+        }
+
+        public override double CalcularArea()
+        {
+            return Base * Altura;
+        }
+    }
+}
