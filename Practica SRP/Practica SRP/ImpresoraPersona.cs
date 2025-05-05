8a4d9c75990b5a1d123f960ad08ef10d2e9d9746
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-05-05 00:30:57 -0400

    Practica SRP completada

---
+++ Practica SRP/Practica SRP/ImpresoraPersona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Practica_SRP
+{
+    public class ImpresoraPersona
+    {
+        public void ImprimirDatos(Persona persona)
+        {
+            Console.WriteLine($"Nombre: {persona.Nombre}");
+            Console.WriteLine($"Edad: {persona.Edad}");
+            Console.WriteLine($"Dirección: {persona.Direccion}");
+            Console.WriteLine($"Correo electrónico: {persona.CorreoElectronico}");
+        }
+
+    }
+}
